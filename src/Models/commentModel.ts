2b6cd7a1af3c1463c6bd439d6a826ec2348c93ec
vladyslav <vladkolesnyk7@gmail.com>
2020-04-07 18:07:02 +0300

    oO

---
+++ src/Models/commentModel.ts
@@ -0,0 +1,7 @@
+class CommentModel {
+  userId: number;
+  id: number;
+  title: string;
+  email: string;
+  body: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {CommentService} from './services/comment.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<app-comment *ngFor="let com of comments" [comment]="com" ></app-comment>`,
+  styles: [],
 })
 export class AppComponent {
   title = 'angular';
+  comments: CommentModel[];
+  constructor(private commentService: CommentService) {
+      this.commentService.getAllComments().subscribe(value => this.comments = value);
+  }
 }
+
